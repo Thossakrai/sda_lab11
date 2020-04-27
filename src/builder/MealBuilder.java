fad5a940b466ffdb374c3ddb128d13d95f0e9e1e
Thossakrai Nakkasem <thossakrai@outlook.com>
2020-04-27 14:36:02 +0700

    initial commit

---
+++ src/builder/MealBuilder.java
@@ -0,0 +1,22 @@
+package builder;/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author boonjv
+ */
+/** Abstract builder for the builder.Meal objects, supporting all the
+ *  operations for building a meal.
+ *
+ * @author YOUR NAME HERE
+ */
+public abstract class MealBuilder {
+
+  protected Meal theMeal;
+
+  // Additional methods here
+
+}
